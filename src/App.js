0d9f18fd24c64b2b6883bfa6a853a56d1d7b7a79
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-09-12 06:55:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import MyStore from './store.js';
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
