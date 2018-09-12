e78e5beea956a69ec76ac3cd3fc3c90ac889178e
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-09-12 07:28:50 -0400

    SUPER simple to show use of observer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react'; //shows the changes made to the store
 import './App.css';
 
 import MyStore from './store.js';
+
 class App extends Component {
+  updateData() {
+    MyStore.myData = 'hello';
+  }
+
+  resetData() {
+    MyStore.myData = 'hi';
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <p>{MyStore.myData}</p>
+        <button onClick={this.updateData}>Click to change</button>
+        <button onClick={this.resetData}>Click to reset</button>
+      </div>
+    );
   }
 }
 
-export default App;
+export default observer(App); //App is told to observe for changes to MyStore
